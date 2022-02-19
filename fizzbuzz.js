e06774b7170e36be7469e7c8d78314323a6ba7a8
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-19 00:49:56 -0800

    if statements

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,14 @@
 // node fizzbuzz.js
+function fizzbuzz(n)
+{
+    for (var i = 1; i <= n; i += 1)
+    {
+        if (i%3 == 0 && i%5 == 0 ){console.log(`${i} FizzBuzz`);}
+        else{console.log(i);}
+    }
+
+}
 
 const prompt = require("prompt-sync")({ sigint: true });
 let n = parseInt(prompt("Enter the number you want to print 1 to :"));
-console.log(n)
+fizzbuzz(n)
