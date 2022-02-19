7636cda0ab0e24872b66a11ff349eb0156a3646c
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-19 00:29:04 -0800

    more

--- discount.js
+++ discount.js
@@ -1,8 +1,12 @@
 // node discount.js
-// function discount(){}
-//     pass
+function discount(original_price, percentage_off)
+{
+    decimal = 100 - percentage_off 
+    decimal = decimal/100
+    console.log(decimal)
+}
+    
 const prompt = require("prompt-sync")({ sigint: true });
-let original_price = (prompt("Enter the original price:"));
-
-// discount()
-console.log(`${original_price}`); 
+let original_price = parseFloat(prompt("Enter the original price: $"));
+let percentage_off = parseFloat(prompt("What is the percentage off: "));
+discount(original_price, percentage_off)
