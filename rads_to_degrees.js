f2a432106c9e8daaca8402175779e7b3a58c51d3
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-19 02:09:58 -0800

    prompts user

--- rads_to_degrees.js
+++ rads_to_degrees.js
@@ -2,3 +2,5 @@
 function rads_to_degrees(rad){}
 
 // formula 4 problem: 1rad × 180/π = 57.296°
+const prompt = require("prompt-sync")({ sigint: true });
+let rad = prompt("Convert your radians to degrees: ");
