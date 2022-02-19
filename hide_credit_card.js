2ec5a96261743ef36686bc7d2eaf0a3bfc4986a8
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-19 01:47:52 -0800

    hidden credit card number

--- hide_credit_card.js
+++ hide_credit_card.js
@@ -1,8 +1,10 @@
 // node hide_credit_card.js
 function hide_credit_card_number(credit_card_number)
 {
-    var multiple_covers= '*'.repeat(3);
-    console.log(multiple_covers + credit_card_number[5,2]);
+    var multiple_covers= '*'.repeat(12);
+    var hidden_credit_card_number = credit_card_number.substring(12,17)
+    console.log(multiple_covers + hidden_credit_card_number);
+    // document.write(hidden_credit_card_number)
 }
 
 const prompt = require("prompt-sync")({ sigint: true });
