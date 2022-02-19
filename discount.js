e048357247c6e1a81f7468d91f1ff18b80b1a9ac
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-19 00:13:40 -0800

    prompts user

---
+++ discount.js
@@ -0,0 +1,8 @@
+// node discount.js
+// function discount(){}
+//     pass
+const prompt = require("prompt-sync")({ sigint: true });
+let original_price = (prompt("Enter the original price:"));
+
+// discount()
+console.log(`${original_price}`); 
