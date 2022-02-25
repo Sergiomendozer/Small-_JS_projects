8296da2c005b443c9fb30d0891ccf163c3b2aba5
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-25 03:24:09 -0800

    rads to degree complete

--- rads_to_degrees.js
+++ rads_to_degrees.js
@@ -1,11 +1,16 @@
 // node rads_to_degrees.js
-function rads_to_degrees(rad)
-{
-    console.log(rad)
+function rads_to_degrees(rad) {
+  degree = rad * (180 / Math.PI);
+  var str_degree = degree.toString();
+  //   var find_dot = ".";
+  var place = str_degree.indexOf(".");
+  place += 3;
+  str_degree = str_degree.substring(0, place);
+
+  console.log(`${str_degree} degrees`);
 }
 
 // formula 4 problem: 1rad × 180/π = 57.296°
 const prompt = require("prompt-sync")({ sigint: true });
-let rad = prompt("Convert your radians to degrees: ");
-// parseint()
-rads_to_degrees(rad)
+let rad = parseInt(prompt("Convert your radians to degrees: "));
+rads_to_degrees(rad);
