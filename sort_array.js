acd8b1984fa9e58594036d51f446a380468b5f4a
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-26 00:12:28 -0800

    sort array

---
+++ sort_array.js
@@ -0,0 +1,11 @@
+// node sort_array.js
+// ascending order
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();
+console.log(fruits);
+
+//descending order
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();
+fruits.reverse();
+console.log(fruits);
